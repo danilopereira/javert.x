fd7f6079df78a8432aebb1e078a0a23bc3971a84
danilopereiraa <danilo.pereira@paulistano.org.br>
2015-09-04 18:59:06 -0300

    Acesso BD mysql

--- src/main/java/com/dev/isengard/app/MyFirstVerticle.java
+++ src/main/java/com/dev/isengard/app/MyFirstVerticle.java
@@ -1,19 +1,57 @@
 package com.dev.isengard.app;
+import com.dev.isengard.repository.config.JDBCConfig;
+
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
 public class MyFirstVerticle extends AbstractVerticle{
 	@Override
-	public void start(Future<Void> fut) throws Exception {
-		vertx
-		.createHttpServer()
-		.requestHandler(r -> {
-			r.response().end("<h1>Hello World from my first Vert.x 3 application</h1>");
-		})
-		.listen(8085, result ->{
-			if (result.succeeded()){
-				fut.complete();
-			}
-			else fut.fail(result.cause());
+	public void start() throws Exception {
+		Router router = Router.router(vertx);	
+
+		Route route = router.route(HttpMethod.GET, "/cep/:cep");
+		HttpServer server = vertx.createHttpServer();
+		route.handler(rountinContext ->{
+			String cep = rountinContext.request().getParam("cep");
+			String cepQuery = cep.substring(0, 5);
+			String cepTest = cepQuery + "-" + cep.substring(5);
+
+			HttpServerResponse r = rountinContext.response();
+
+
+			JDBCClient client = new JDBCConfig().client;
+
+			client.getConnection(r2->{
+				if(r2.succeeded()){
+					SQLConnection connection = r2.result();
+
+					connection.query("SELECT UF FROM uf WHERE '"+cepQuery+"' BETWEEN Cep1 AND Cep2", res->{
+						ResultSet rs = res.result();
+						JsonArray ufS = rs.getResults().get(0);
+						String uf = ufS.toString().toLowerCase().substring(2, 4);
+						connection.query("SELECT * FROM "+uf+" WHERE cep = '"+cepTest+"'", resFinal->{
+							System.out.println(cepTest);
+							ResultSet result = resFinal.result();
+							r.putHeader("content-type", "application/json").end(result.toJson().toString());
+						});
+
+
+					});
+				}
+			});
+
 		});
+
+		server.requestHandler(router::accept).listen(8085);
+
+
+
 	}
 }
--- src/main/java/com/dev/isengard/repository/config/JDBCConfig.java
+++ src/main/java/com/dev/isengard/repository/config/JDBCConfig.java
@@ -6,10 +6,13 @@ import io.vertx.ext.jdbc.JDBCClient;
 
 public class JDBCConfig{
 	
-	JsonObject config = new JsonObject();
+	JsonObject config = new JsonObject()
+			.put("url", "jdbc:mysql://localhost:3306/cep")
+			.put("user", "root")
+			.put("password", "")
+			.put("driver_class", "com.mysql.jdbc.Driver")
+			.put("max_pool_size", 30);
 	
-	JDBCClient client = JDBCClient.createShared(Vertx.vertx(), config);	
-	
-	
+	public JDBCClient client = JDBCClient.createShared(Vertx.vertx(), config);	
 
 }
